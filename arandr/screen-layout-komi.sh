193039cf7d32d8c91acd9db414b3e551500a787b
nymwa <nymwa0@gmail.com>
2019-11-10 20:15:45 +0900

    ≡╹ω╹≡

--- arandr/screen-layout-komi.sh
+++ arandr/screen-layout-komi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-1 --off --output DisplayPort-0 --off --output DisplayPort-2 --mode 1360x768 --pos 0x0 --rotate normal --output DVI-D-0 --off --output HDMI-A-0 --off
+xrandr --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --off --output DVI-D-0 --off
